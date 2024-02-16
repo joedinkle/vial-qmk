34605ecab6678dc9cc66c0bb0f0d6a9e4edf5a2b
joedinkle <eodkeyboards@gmail.com>
2024-02-15 20:24:47 -0800

    Vixen and Blvd updates prior to catching main branch up with upstream

--- keyboards/eyeohdesigns/theboulevard/theboulevard.h
+++ keyboards/eyeohdesigns/theboulevard/theboulevard.h
@@ -167,3 +167,18 @@
     { k30, k31, KC_NO,   k33,   k34,   k35,   k36,   k37,   k38,   k39,   k3a, k3b, k3c }, \
     { k40, k41, k42,   KC_NO, KC_NO, KC_NO, KC_NO, k47,   KC_NO, KC_NO, KC_NO, k4b, k4c }\
 }
+
+#define LAYOUT( \
+    k00, k01,                                                           k0a, k0b, k0c, \
+    k10, k11,   k12,   k13,   k14,   k15,   k16,   k17,   k18,   k19,   k1a, k1b, k1c, \
+    k20, k21,   k22,   k23,   k24,   k25,   k26,   k27,   k28,   k29,   k2a, k2b, k2c, \
+    k30, k31,   k32,   k33,   k34,   k35,   k36,   k37,   k38,   k39,   k3a, k3b, k3c, \
+    k40, k41,   k42,   k43,   k44,   k45,   k46,   k47,   k48,   k49,   k4a, k4b, k4c  \
+) \
+{ \
+    { k00, k01, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, k0a, k0b, k0c },\
+    { k10, k11, k12,   k13,   k14,   k15,   k16,   k17,   k18,   k19,   k1a, k1b, k1c },\
+    { k20, k21, k22,   k23,   k24,   k25,   k26,   k27,   k28,   k29,   k2a, k2b, k2c },\
+    { k30, k31, k32,   k33,   k34,   k35,   k36,   k37,   k38,   k39,   k3a, k3b, k3c }, \
+    { k40, k41, k42,   k43,   k44,   k45,   k46,   k47,   k48,   k49,   k4a, k4b, k4c }\
+}
